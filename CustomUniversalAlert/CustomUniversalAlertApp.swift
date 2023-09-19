13929e265d2f579888315cebbcfec7e0b2e9fd09
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-19 14:28:55 +0300

    Initial Commit

---
+++ CustomUniversalAlert/CustomUniversalAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomUniversalAlertApp.swift
+//  CustomUniversalAlert
+//
+//  Created by Yunus Emre Taşdemir on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomUniversalAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
